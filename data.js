e2b922ad47159a1c4f1ba7a3d47cf4c2c077333b
ha-king <003king@gmail.com>
2020-11-05 15:26:55 -0600

    updating code

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ var data = [
       {
         "points": 100,
         "question": "It's easier to ask forgiveness than it is _______________",
-        "answer": "To get some teams to use best-practice..."
+        "answer": "this is awesome stuff..."
       },
       {
         "points": 200,
